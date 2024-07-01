fb42b3b08c9688ce36407dbcca0c2b6f1279260b
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-07-01 18:16:23 +0700

    Database có dữ liệu mẫu

--- Tài liệu/BackUp_Database/nghiencuukhoahoc_01_07_2024.sql
+++ Tài liệu/BackUp_Database/nghiencuukhoahoc_01_07_2024.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 01, 2024 lúc 12:55 PM
+-- Thời gian đã tạo: Th7 01, 2024 lúc 01:15 PM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.0.30
 
