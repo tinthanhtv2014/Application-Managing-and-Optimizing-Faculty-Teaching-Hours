e73d1af9e31f00dcd12b7e65e5b276c47da0e8b2
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-07-29 19:43:50 +0700

    update api chi tiết giảng viên chưa chọn khung

--- backend-nghiencuukhoahoc/src/controllers/TruongbomonController/giangvienCONTROLLER.js
+++ backend-nghiencuukhoahoc/src/controllers/TruongbomonController/giangvienCONTROLLER.js
@@ -1,6 +1,7 @@
 const {
   xem_giangvien,
   xem_giangvien_dachonkhung,
+  xem_giangvien_chuachonkhung,
 } = require("../../services/TruongbomonServices/CRUDgiangvienbomon");
 
 const get_giangvien_CNTT = async (req, res) => {
@@ -47,7 +48,30 @@ const get_giangvien_CNTT_da_chon_khung = async (req, res) => {
   }
 };
 
+const get_giangvien_CNTT_chua_chon_khung = async (req, res) => {
+  try {
+    let page = req.query.page;
+    let limit = req.query.limit;
+    let TENBOMON = req.query.TENBOMON;
+    let results = await xem_giangvien_chuachonkhung(page, limit, TENBOMON);
+
+    return res.status(200).json({
+      EM: results.EM,
+      EC: results.EC,
+      DT: results.DT,
+    });
+  } catch (error) {
+    console.log(error);
+    // return res.status(200).json({
+    //   EM: results.EM,
+    //   EC: results.EC,
+    //   DT: results.DT,
+    // });
+  }
+};
+
 module.exports = {
   get_giangvien_CNTT,
   get_giangvien_CNTT_da_chon_khung,
+  get_giangvien_CNTT_chua_chon_khung,
 };
--- backend-nghiencuukhoahoc/src/services/TruongbomonServices/CRUDgiangvienbomon.js
+++ backend-nghiencuukhoahoc/src/services/TruongbomonServices/CRUDgiangvienbomon.js
@@ -171,15 +171,7 @@ LIMIT
     );
 
     const totalCountResult = await pool.execute(
-      `SELECT 
-    bm.TENBOMON, 
-    gv.TENGV, 
-    gv.EMAIL, 
-    cd.TENCHUCDANH, 
-    cv.TENCHUCVU,  
-    tk.TRANGTHAITAIKHOAN,
-    nh.TENNAMHOC, 
-    kic.*
+      `SELECT COUNT(*) AS total
 FROM 
     taikhoan AS tk
 JOIN 
@@ -265,7 +257,130 @@ WHERE
   }
 };
 
+const xem_giangvien_chuachonkhung = async (page, limit, TENBOMON) => {
+  if (page && limit) {
+    let offset = (page - 1) * limit;
+    let [results0, fields] = await pool.execute(
+      `SELECT 
+    k.TENKHOA, 
+    bm.TENBOMON, 
+    tk.TENDANGNHAP, 
+    gv.TENGV, 
+    gv.EMAIL, 
+    cd.TENCHUCDANH, 
+    cv.TENCHUCVU, 
+    tk.PHANQUYEN, 
+    tk.TRANGTHAITAIKHOAN
+FROM 
+    taikhoan AS tk
+JOIN 
+    giangvien AS gv ON tk.MAGV = gv.MAGV
+JOIN 
+    bomon AS bm ON bm.MABOMON = gv.MABOMON
+JOIN 
+    khoa AS k ON k.MAKHOA = bm.MAKHOA
+LEFT JOIN 
+    chon_khung AS ck ON gv.MAGV = ck.MAGV
+LEFT JOIN 
+    giu_chuc_vu AS gcv ON gv.MAGV = gcv.MAGV
+LEFT JOIN 
+    chucvu AS cv ON gcv.MACHUCVU = cv.MACHUCVU
+LEFT JOIN 
+    co_chuc_danh AS ccd ON ccd.MAGV = gv.MAGV
+LEFT JOIN 
+    chucdanh AS cd ON ccd.MACHUCDANH = cd.MACHUCDANH
+WHERE 
+    bm.TENBOMON = ? AND ck.MAGV IS NULL
+ORDER BY 
+    tk.TENDANGNHAP ASC
+LIMIT 
+    ? OFFSET ?;
+`,
+      [TENBOMON, limit, offset]
+    );
+
+    const totalCountResult = await pool.execute(
+      `SELECT COUNT(*) AS total
+FROM 
+    taikhoan AS tk
+JOIN 
+    giangvien AS gv ON tk.MAGV = gv.MAGV
+JOIN 
+    bomon AS bm ON bm.MABOMON = gv.MABOMON
+JOIN 
+    khoa AS k ON k.MAKHOA = bm.MAKHOA
+LEFT JOIN 
+    chon_khung AS ck ON gv.MAGV = ck.MAGV
+LEFT JOIN 
+    giu_chuc_vu AS gcv ON gv.MAGV = gcv.MAGV
+LEFT JOIN 
+    chucvu AS cv ON gcv.MACHUCVU = cv.MACHUCVU
+LEFT JOIN 
+    co_chuc_danh AS ccd ON ccd.MAGV = gv.MAGV
+LEFT JOIN 
+    chucdanh AS cd ON ccd.MACHUCDANH = cd.MACHUCDANH
+WHERE 
+    bm.TENBOMON = ? AND ck.MAGV IS NULL`,
+      [TENBOMON]
+    );
+
+    const totalCount = totalCountResult[0][0].total;
+    let totalPages = Math.ceil(totalCount / limit);
+    let data = {
+      totalRows: results0,
+      totalPages: totalPages,
+      users: fields,
+    };
+    return {
+      EM: "Xem thông tin giảng viên thành công",
+      EC: 1,
+      DT: data,
+    };
+  } else {
+    let [results0, fields] = await pool.execute(
+      `SELECT 
+    k.TENKHOA, 
+    bm.TENBOMON, 
+    tk.TENDANGNHAP, 
+    gv.TENGV, 
+    gv.EMAIL, 
+    cd.TENCHUCDANH, 
+    cv.TENCHUCVU, 
+    tk.PHANQUYEN, 
+    tk.TRANGTHAITAIKHOAN
+FROM 
+    taikhoan AS tk
+JOIN 
+    giangvien AS gv ON tk.MAGV = gv.MAGV
+JOIN 
+    bomon AS bm ON bm.MABOMON = gv.MABOMON
+JOIN 
+    khoa AS k ON k.MAKHOA = bm.MAKHOA
+LEFT JOIN 
+    chon_khung AS ck ON gv.MAGV = ck.MAGV
+LEFT JOIN 
+    giu_chuc_vu AS gcv ON gv.MAGV = gcv.MAGV
+LEFT JOIN 
+    chucvu AS cv ON gcv.MACHUCVU = cv.MACHUCVU
+LEFT JOIN 
+    co_chuc_danh AS ccd ON ccd.MAGV = gv.MAGV
+LEFT JOIN 
+    chucdanh AS cd ON ccd.MACHUCDANH = cd.MACHUCDANH
+WHERE 
+    bm.TENBOMON = ? AND ck.MAGV IS NULL`,
+      [TENBOMON]
+    );
+
+    return {
+      EM: "Xem thông tin giảng viên thành công",
+      EC: 1,
+      DT: results0,
+    };
+  }
+};
+
 module.exports = {
   xem_giangvien,
   xem_giangvien_dachonkhung,
+  xem_giangvien_chuachonkhung,
 };
