98aa4e0f6d902d2155d476ed63c3b9bc6b9c734a
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-08-13 20:50:25 +0700

    update SOGIOQUYDOI

--- frontend-nghiencuukhoahoc/src/TruongBoMon/dashbroand/src/layouts/dashboard/config-navigation.jsx
+++ frontend-nghiencuukhoahoc/src/TruongBoMon/dashbroand/src/layouts/dashboard/config-navigation.jsx
@@ -1,40 +1,44 @@
-import SvgColor from '../../components/svg-color';
+import SvgColor from "../../components/svg-color";
 
 // ----------------------------------------------------------------------
 
 const icon = (name) => (
-  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+  <SvgColor
+    src={`/assets/icons/navbar/${name}.svg`}
+    sx={{ width: 1, height: 1 }}
+  />
 );
 
 const navConfig = [
   {
-    title: 'Thống kê',
-    path: '/',
-    icon: icon('ic_analytics'),
+    title: "Thống kê",
+    path: "/",
+    icon: icon("ic_analytics"),
   },
   {
-    title: 'Danh sách giảng viên',
-    path: '/giang-vien',
-    icon: icon('ic_user'),
+    title: "Danh sách giảng viên",
+    path: "/giang-vien",
+    icon: icon("ic_user"),
   },
   {
-    title: 'Phân công giảng dạy',
-    path: '/quan-ly-phan-cong-giang-day',
-    icon: icon('ic_cart'),
+    title: "Chương Trình Đào Tạo",
+    path: "/quan-ly-phan-cong-giang-day",
+    icon: icon("ic_cart"),
   },
   {
-    title: 'Gợi ý phân công giảng viên',
-    path: '/goi-y-phan-cong-giang-vien',
-    icon: icon('ic_blog'),
-  }, {
-    title: 'Đăng Ký Khung Giờ Chuẩn',
-    path: '/dang-ky-khung-gio-chuan',
-    icon: icon('ic_blog'),
+    title: "Gợi ý phân công giảng viên",
+    path: "/goi-y-phan-cong-giang-vien",
+    icon: icon("ic_blog"),
   },
   {
-    title: 'Theo dõi đăng ký',
-    path: '/theo-doi-dang-ky',
-    icon: icon('ic_blog'),
+    title: "Đăng Ký Khung Giờ Chuẩn",
+    path: "/dang-ky-khung-gio-chuan",
+    icon: icon("ic_blog"),
+  },
+  {
+    title: "Theo dõi đăng ký",
+    path: "/theo-doi-dang-ky",
+    icon: icon("ic_blog"),
   },
 ];
 
