85a55c5442ad5a3f211384c958ee58ea32dca8e2
Minurte1 <hohoangphucjob@gmail.com>
2024-08-28 22:39:24 +0700

    change password account

---
+++ frontend-nghiencuukhoahoc/src/Admin/dashbroand/src/sections/Account/component/changePassword.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+
+import CookiesAxios from "../../CookiesAxios";
+import { toast } from "react-toastify";
+const ChangePasswordForm = ({ data_user }) => {
+  const [matKhauCu, setMatKhauCu] = useState(null);
+  const [nhapLaiMatKhauMoi, setNhapLaiMatKhauMoi] = useState(null);
+  const [matKhauMoi, setMatKhauMoi] = useState(null);
+  const [error, setError] = useState("");
+  console.log("check data_user =>", data_user);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (nhapLaiMatKhauMoi === matKhauMoi && data_user.taikhoan && matKhauCu) {
+      try {
+        const response = await CookiesAxios.put(
+          `${process.env.REACT_APP_URL_SERVER}/api/v1/admin/taikhoan/sua/${data_user.taikhoan}`,
+          {
+            matKhaucu: matKhauCu,
+            phanQuyen: data_user.phanquyen,
+            matKhaumoi: matKhauMoi,
+          }
+        );
+        console.log(response.data);
+        if (response.data.EC === 1) {
+          alert("Thay đổi mật khẩu thành công!");
+        } else {
+          setError("Thay đổi mật khẩu thất bại. Vui lòng kiểm tra lại.");
+        }
+      } catch (error) {
+        setError("Có lỗi xảy ra. Vui lòng thử lại sau.");
+      }
+    } else {
+      setError("Mật Khẩu Của Bạn Có Vấn Đề Hihi");
+    }
+  };
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ maxWidth: 400, margin: "auto", mt: 4 }}
+    >
+      <TextField
+        fullWidth
+        label="Mật khẩu cũ"
+        type="password"
+        value={matKhauCu}
+        onChange={(e) => setMatKhauCu(e.target.value)}
+        margin="normal"
+        required
+      />
+
+      <TextField
+        fullWidth
+        label="Mật khẩu mới"
+        type="password"
+        value={matKhauMoi}
+        onChange={(e) => setMatKhauMoi(e.target.value)}
+        margin="normal"
+        required
+      />
+      <TextField
+        fullWidth
+        label="Nhập Lại Mật khẩu mới"
+        type="password"
+        value={nhapLaiMatKhauMoi}
+        onChange={(e) => setNhapLaiMatKhauMoi(e.target.value)}
+        margin="normal"
+        required
+      />
+      {error && <Box sx={{ color: "red", mt: 2 }}>{error}</Box>}
+
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        fullWidth
+        sx={{ mt: 3 }}
+      >
+        Đổi mật khẩu
+      </Button>
+    </Box>
+  );
+};
+
+export default ChangePasswordForm;
