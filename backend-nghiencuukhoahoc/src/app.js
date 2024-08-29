90ef0c97b5e57e0eb4fd7d762b3aed4da1ce605b
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-08-30 06:00:11 +0700

    test time trước và sau khi dùng

--- backend-nghiencuukhoahoc/src/app.js
+++ backend-nghiencuukhoahoc/src/app.js
@@ -70,32 +70,34 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-// const cluster = require('cluster');
-// const http = require('http');
-// const os = require('os');
-// const express = require('express');
-// require('dotenv').config();
-// const cors = require('cors');
-// const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const bodyParser = require('body-parser');
-// const configViewEngine = require('./config/ViewEngine');
+// -----------------------------------------------------------------------------------------------------------------------
+
+// const cluster = require("cluster");
+// const http = require("http");
+// const os = require("os");
+// const express = require("express");
+// require("dotenv").config();
+// const cors = require("cors");
+// const path = require("path");
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// const configViewEngine = require("./config/ViewEngine");
 
 // // Router =========================================================================
-// const CRUDTaiKhoan = require('./routes/AdminRoute/adminRoute');
-// const CRUDKhoa = require('./routes/AdminRoute/KhoaRoute');
-// const CRUDBoMon = require('./routes/AdminRoute/BoMonRouter');
-// const CRUDGiangVien = require('./routes/AdminRoute/GiangVienRouter');
-// const CRUDMonHoc = require('./routes/AdminRoute/MonHocRouter');
-// const CRUDNamHoc = require('./routes/AdminRoute/NamHocRouter');
-// const CRUDgiangvien_CNTT = require('./routes/TruongbomonRoute/giangvienRoute');
-// const CRUDchuongtrinh_CNTT = require('./routes/TruongbomonRoute/chuongtrinhRoute');
-// const QuyenGiangVienRouter = require('./routes/GiangvienRoute/QuyenGiangVienRouter');
-// const CURDanhMuc = require('./routes/AdminRoute/DanhMucRoute');
-// const TruongKhoaRoute = require('./routes/TruongkhoaRoute/TruongKhoaRoute');
-// const CRUDHockinienkhoa = require('./routes/AdminRoute/hockinienkhoaRoute');
-// const DangKyDanhMucRoute = require('./routes/GiangvienRoute/DangKyDanhMucSpKHCNRoute');
-// const CRUDHinhthucdanhgia = require('./routes/AdminRoute/hinhthucdanhgiaRoute');
+// const CRUDTaiKhoan = require("./routes/AdminRoute/adminRoute");
+// const CRUDKhoa = require("./routes/AdminRoute/KhoaRoute");
+// const CRUDBoMon = require("./routes/AdminRoute/BoMonRouter");
+// const CRUDGiangVien = require("./routes/AdminRoute/GiangVienRouter");
+// const CRUDMonHoc = require("./routes/AdminRoute/MonHocRouter");
+// const CRUDNamHoc = require("./routes/AdminRoute/NamHocRouter");
+// const CRUDgiangvien_CNTT = require("./routes/TruongbomonRoute/giangvienRoute");
+// const CRUDchuongtrinh_CNTT = require("./routes/TruongbomonRoute/chuongtrinhRoute");
+// const QuyenGiangVienRouter = require("./routes/GiangvienRoute/QuyenGiangVienRouter");
+// const CURDanhMuc = require("./routes/AdminRoute/DanhMucRoute");
+// const TruongKhoaRoute = require("./routes/TruongkhoaRoute/TruongKhoaRoute");
+// const CRUDHockinienkhoa = require("./routes/AdminRoute/hockinienkhoaRoute");
+// const DangKyDanhMucRoute = require("./routes/GiangvienRoute/DangKyDanhMucSpKHCNRoute");
+// const CRUDHinhthucdanhgia = require("./routes/AdminRoute/hinhthucdanhgiaRoute");
 // //=================================================================================
 
 // const corsOptions = {
@@ -106,51 +108,51 @@ app.listen(port, () => {
 // const numCPUs = os.cpus().length;
 
 // if (cluster.isMaster) {
-//     console.log(`Master ${process.pid} is running`);
+//   console.log(`Master ${process.pid} is running`);
 
-//     // Fork workers.
-//     for (let i = 0; i < numCPUs; i++) {
-//         cluster.fork();
-//     }
+//   // Fork workers.
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
 
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`Worker ${worker.process.pid} died`);
-//         cluster.fork(); // Restart a new worker if one dies
-//     });
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`Worker ${worker.process.pid} died`);
+//     cluster.fork(); // Restart a new worker if one dies
+//   });
 // } else {
-//     const app = express();
-//     const port = process.env.PORT || 3307;
+//   const app = express();
+//   const port = process.env.PORT || 3307;
 
-//     app.use(cors(corsOptions));
-//     app.use(cookieParser());
-//     app.use(express.json({ limit: '50mb' }));
-//     app.use(express.urlencoded({ limit: '50mb', extended: true }));
-//     app.use(bodyParser.urlencoded({ extended: false }));
-//     app.use(bodyParser.json());
+//   app.use(cors(corsOptions));
+//   app.use(cookieParser());
+//   app.use(express.json({ limit: "50mb" }));
+//   app.use(express.urlencoded({ limit: "50mb", extended: true }));
+//   app.use(bodyParser.urlencoded({ extended: false }));
+//   app.use(bodyParser.json());
 
-//     configViewEngine(app);
+//   configViewEngine(app);
 
-//     // Route configuration
-//     CRUDTaiKhoan(app);
-//     CRUDKhoa(app);
-//     CRUDBoMon(app);
-//     CRUDGiangVien(app);
-//     CRUDMonHoc(app);
-//     CRUDNamHoc(app);
-//     CRUDgiangvien_CNTT(app);
-//     QuyenGiangVienRouter(app);
-//     CRUDchuongtrinh_CNTT(app);
-//     TruongKhoaRoute(app);
-//     CURDanhMuc(app);
-//     CRUDHockinienkhoa(app);
-//     DangKyDanhMucRoute(app);
-//     CRUDHinhthucdanhgia(app);
+//   // Route configuration
+//   CRUDTaiKhoan(app);
+//   CRUDKhoa(app);
+//   CRUDBoMon(app);
+//   CRUDGiangVien(app);
+//   CRUDMonHoc(app);
+//   CRUDNamHoc(app);
+//   CRUDgiangvien_CNTT(app);
+//   QuyenGiangVienRouter(app);
+//   CRUDchuongtrinh_CNTT(app);
+//   TruongKhoaRoute(app);
+//   CURDanhMuc(app);
+//   CRUDHockinienkhoa(app);
+//   DangKyDanhMucRoute(app);
+//   CRUDHinhthucdanhgia(app);
 
-//     app.get('/', (req, res) => {
-//         res.send('Hello World!');
-//     });
+//   app.get("/", (req, res) => {
+//     res.send("Hello World!");
+//   });
 
-//     app.listen(port, () => {
-//         console.log(`Worker ${process.pid} started on http://localhost:${port}`);
-//     });
+//   app.listen(port, () => {
+//     console.log(`Worker ${process.pid} started on http://localhost:${port}`);
+//   });
 // }
