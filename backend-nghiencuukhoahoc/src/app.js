78d381f2efa21accbbe7ff29646a8af8fd5b6b28
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-08-30 05:33:58 +0700

    thêm data cho bộ môn điện gì đó và thêm limit vào file server.js

--- backend-nghiencuukhoahoc/src/app.js
+++ backend-nghiencuukhoahoc/src/app.js
@@ -36,8 +36,8 @@ app.use(cors(corsOptions));
 // app.use(cors());
 
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
