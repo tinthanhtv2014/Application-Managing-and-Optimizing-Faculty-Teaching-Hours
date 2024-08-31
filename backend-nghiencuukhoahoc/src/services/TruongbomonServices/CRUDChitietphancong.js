71b110a0ca9ecc03e82acf88da1f4ed4e7b4376c
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-09-01 06:26:01 +0700

    thiếu thông tin viết data excel

--- backend-nghiencuukhoahoc/src/services/TruongbomonServices/CRUDChitietphancong.js
+++ backend-nghiencuukhoahoc/src/services/TruongbomonServices/CRUDChitietphancong.js
@@ -2,14 +2,29 @@ const pool = require("../../config/database");
 
 const createchitietphancong_excel = async (datachitietphancong) => {
   try {
+    let results = [];
+    // console.log("check results =>", dataChuongtrinhdaotaoExcelArray);
+    // Kiểm tra thông tin trong file excel
+    for (var i = 0; i < datachitietphancong.length; i++) {}
+
+    // Bắt đầu tạo tài khoản
+    for (var i = 0; i < datachitietphancong.length; i++) {
+      results.push({
+        EM: `Tạo phân công thành công`,
+        EC: 0,
+        DT: [],
+      });
+    }
+
     return {
-      EM: "Xem thông tin chương trình đào tạo thành công",
+      EM: "Tất cả chi tiết đã được tạo",
       EC: 1,
-      DT: results_ctdt_bomon,
+      DT: results,
     };
   } catch (error) {
+    console.log("Lỗi services createchitietphancong_excel", error);
     return {
-      EM: "Lỗi services createchitietphancong_excel",
+      EM: "Lỗi services createTaiKhoanExcel",
       EC: -1,
       DT: [],
     };
