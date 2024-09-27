3c4f5ff6259e6041c3fe6499b8b982650150bea9
BaoQuocZero <130838918+BaoQuocZero@users.noreply.github.com>
2024-09-27 23:24:50 +0700

    python app.py http://127.0.0.1:5000/

---
+++ python-server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Định nghĩa một route đơn giản
+@app.route('/', methods=['GET'])
+def chao():
+    return jsonify(message="Xin chao!")
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify(message="Hello, World!")
+
+# Chạy server
+if __name__ == '__main__':
+    app.run(debug=True)
